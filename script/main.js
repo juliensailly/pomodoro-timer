6d0608cd72accf49ab55889921049f9230f2ee95
Julien Sailly <juliensailly76@gmail.com>
2023-09-08 16:38:03 +0200

    Code reorganisation and bug fix

--- script/main.js
+++ script/main.js
@@ -5,7 +5,9 @@ var workTime = 25,
     isPaused = true,
     isRunning = false,
     currentInterval = null;
-const timerElement = document.getElementById("timer"), startButton = document.getElementById("startButton"), resetButton = document.getElementById("resetButton");
+const timerElement = document.getElementById("timer"),
+    startButton = document.getElementById("startButton"),
+    resetButton = document.getElementById("resetButton");
 resetButton.style.display = "none";
 
 // A function used to start the timer
@@ -25,7 +27,6 @@ function startTimer() {
 
         updateTimer();
         currentInterval = setInterval(() => updateTimer(), 1000);
-        
     } else {
         startButton.textContent = "Resume";
         resetButton.style.display = "inline-block";
@@ -36,8 +37,6 @@ function startTimer() {
 // A function used to update the timer
 function updateTimer() {
     currentTime = currentTime <= 0 ? 0 : currentTime - 1;
-
-    console.log(currentTime);
 
     if (currentTime <= 0) {
         isWork = !isWork;
@@ -54,7 +53,7 @@ function updateTimer() {
 
 // A function used to update UI and play a sound when changing state
 function updateUI() {
-
+    
     let minutes = parseInt(currentTime / 60, 10);
     let secondes = parseInt(currentTime % 60, 10);
 
@@ -86,16 +85,13 @@ function updateUI() {
 function resetTimer() {
     isPaused = true;
     isWork = true;
+    isRunning = false;
     startButton.textContent = "Start";
     resetButton.style.display = "none";
     clearInterval(currentInterval);
     currentTime = workTime * 60;
 
-    let minutes = parseInt(currentTime / 60, 10);
-    let secondes = parseInt(currentTime % 60, 10);
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    secondes = secondes < 10 ? "0" + secondes : secondes;
-    timerElement.innerText = `${minutes}:${secondes}`;
+    updateUI();
 }
 
 // A function used to decrease the timer value
@@ -113,19 +109,20 @@ function monitorTimesInputs() {
         }
         localStorage.setItem("workTime", workTime);
     });
-    document.getElementById("breakTime").addEventListener("change", function () {
-        breakTime = this.value;
-        if (!isWork) {
-            currentTime = breakTime * 60;
-            updateUI();
-        }
-        localStorage.setItem("breakTime", breakTime);
-    });
+    document
+        .getElementById("breakTime")
+        .addEventListener("change", function () {
+            breakTime = this.value;
+            if (!isWork) {
+                currentTime = breakTime * 60;
+                updateUI();
+            }
+            localStorage.setItem("breakTime", breakTime);
+        });
 }
 
 // A function used to initialize the page
 function onPageLoad() {
-    updateUI();
     monitorTimesInputs();
 
     if (localStorage.getItem("workTime") != null) {
@@ -139,11 +136,7 @@ function onPageLoad() {
 
     currentTime = workTime * 60;
 
-    let minutes = parseInt(currentTime / 60, 10);
-    let secondes = parseInt(currentTime % 60, 10);
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    secondes = secondes < 10 ? "0" + secondes : secondes;
-    timerElement.innerText = `${minutes}:${secondes}`;
+    updateUI();
 }
 
 onPageLoad();
